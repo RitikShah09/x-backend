72be754374c8a5307a7693ad7206951e244f4067
RitikShah09 <ritiksbs@gmail.com>
2024-07-19 03:16:00 +0530

    updated

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,11 +4,13 @@ const ErrorHandler = require("../utils/errorHandler");
 const { catchAsyncErrors } = require("./catchAsyncErrors");
 
 exports.isAuthenticated = catchAsyncErrors(async (req, res, next) => {
-    const { token } = req.cookies;
-    if (!token) {
-        return next(new ErrorHandler("Please Login To Access The Resources", 401));
-    }
-    const { id } = jwt.verify(token, process.env.JWT_SECRET);
-    req.id = id;
-    next();
-})
+  // const { token } = req.cookies;
+  const token =
+    req.cookies.token || req.body.headers.Authorization.split(" ")[1];
+  if (!token) {
+    return next(new ErrorHandler("Please Login To Access The Resources", 401));
+  }
+  const { id } = jwt.verify(token, process.env.JWT_SECRET);
+  req.id = id;
+  next();
+});
